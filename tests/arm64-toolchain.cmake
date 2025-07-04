4218aa726de55ceb1915fcf0e206b58b2941d90f
haozen <haozen@qti.qualcomm.com>
2025-07-04 15:53:26 +0800

    Summary: - Exclude files unrelated to source code.💡

--- tests/arm64-toolchain.cmake
+++ tests/arm64-toolchain.cmake
@@ -10,7 +10,7 @@ set(CMAKE_LINKER "C:/Users/haozen/software/clang+llvm-20.1.7-x86_64-pc-windows-m
 
 # 指定 Windows SDK 路径（根据你安装的版本调整）
 set(CMAKE_INCLUDE_PATH "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/um")
-set(CMAKE_LIBRARY_PATH "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/arm64")
+set(CMAKE_LIBRARY_PATH "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/um/arm64")
 
 # 指定目标架构为 ARM64 Windows
 set(CMAKE_C_FLAGS "--target=arm64-windows-msvc")
